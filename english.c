46df76a52f7522c055c817aee75d07fad5f3ee26
dangminhtung <minhtungd2402@gmail.com>
2026-01-24 02:42:23 +0700

    trave

--- english.c
+++ english.c
@@ -1,4 +1,34 @@
 /*
+Vietnamese people often use mobile phones that can be used when moving, such as Koreans. Currently, cel phones are very convenient not only because they can make phone calls or send text messages, hut also because when they are connected to the internet, they can use a variety of useful services, whether surfing the Internet, watching movies, or shopping on-ine. In particular,if you download a convenient application that you can do with your phone, you can simply use it from games to taxi orders by using the application. For example, if you download Internet banking on your cell phone, you can do all the banking work without going to the bank, so Vietnamese people think that cell phones should not be absent from their
+
+* Vietnamese people often use mobile phones every day. They are very convenient and easy to use. People can make calls and send messages. They can also use the internet to watch videos and shop online. With some apps, people can order a taxi or use internet banking. So, mobile phones are very important for Vietnamese people.
+
+* In the past, I used a keyboard phone. It could only make calls and send messages. The screen was small, so I didn’t like it. Now, I use a smartphone with a touch screen. It is big and beautiful. I can make video calls to see my friends and family, so I feel they are close to me.
+
+* Many things happened when I used my cell phone. The most memorable thing was when I changed my phone. A few months ago, my old keyboard phone broke, so I bought a new smartphone. At first, I didn’t know how to unlock it, so I couldn’t use it for one week. Later, my friend taught me how to use fingerprint unlock. Now I use it every day, and it is very fast and convenient.
+
+* I first used the internet when I was in middle school. At that time, it was inconvenient because I had to go to a PC room. There was not much information, so it took a long time to search. Now the internet has developed a lot. There are many websites with a lot of information. Also, with smartphones, I can use the internet anytime without going to a PC room.
+
+* Many things happened when I used Facebook, but one was the most memorable. One day, I posted a video about an accident. Many people watched it and left comments. At first, people believed it was real. Later, I told them it was acting. People were angry, so I apologized and deleted the video. Now, I always think carefully before I post anything online.
+
+* I use SNS to meet new friends and find old friends. I often use Facebook and KakaoTalk. With a smartphone, I can use SNS anytime and anywhere. I can read news, play games, and chat with people. I can also meet people who have similar interests. For me, SNS is a good way to connect with others.
+
+* SNS is very important in my daily life. I often use Facebook. I can post pictures and videos and share my interests. My friends can leave messages or make video calls. It is very easy and convenient to contact my friends on Facebook.
+
+* Using the internet has many advantages, but it also has problems. Sometimes, there is wrong information online. People may believe it and make mistakes. Also, some people experience cyberbullying. When shopping online, I cannot see the product, so the quality can be bad. So, I think we should be careful when using the internet.
+
+* Last week, I bought new clothes. After the first wash, the color changed and the sleeves were stretched. I called the store and complained. The owner said it was washable, but I didn’t agree. In the end, I returned the clothes. After this experience, I always check carefully before I buy something.
+
+* In the past, I used a keyboard phone. It could only make calls and send messages. A few months ago, I changed to a smartphone. I can take clear pictures and videos anytime. I can also watch movies on the internet. I am very satisfied with my smartphone.
+
+* I have a new computer with a big monitor. When I watch movies, I feel like I am in a movie theater. The screen is clear, so my eyes are not tired. The sound is also very good. After a hard day, I relax by watching movies and listening to music on my computer.
+
+
+In the past, I used to use a keyboard phone that could only make phone calls or send text messages, but it wasn't pretty because the screen was small and I was really scared. In that sense, I didn't like old cell phones, so I recently changed to a new smartphone with a touch screen. Touch smartphones are not only very large but also very beautiful because they are thin. In particular, rather than an old keyboard phone that only listens to sound when calling, this smartphone can make video calls to see and talk to others in person through the screen of the smartphone, so whenever you want to talk to your friend or miss your family living in a distant hometown, you Security 1P/SRV Đang 9 can make video calls and meet them, so I think they will be next to me. It's very good.
+
+There were many things that happened when I used my cell phone, but one of them is the most memorable. A few months ago, my old keyboard phone broke down, so I changed it to a smartphone with a touch screen. However, this new smartphone does not use the same number as the old keyboard phone, so it was salty to know how to open the smartphone. So l opened the phone lock in various aways, but it failed, so I couldn't use my smartphone until a week later. After that, a friend taught me how to curl with fingerprint recognition function, so I found out. Now that I use my smartphone a lot, I'm very familiar with the truth recognition function. Not only is the method fast, but it is also very convenient because it keeps smartphone security well.
+
+
 as a matter of fact: thực tế là
 
 aren't : ót
