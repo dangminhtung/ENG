72ff76202a494790bed88e5a4500303c49e5d059
dangminhtung <79414259+dangminhtung@users.noreply.github.com>
2026-01-27 00:24:35 +0700

    update

--- english/travel.c
+++ english/travel.c
@@ -1,19 +1,16 @@
 /*
 
 
-* There are many famous places in Vietnam, but my favorite is Da Nang. It has beautiful beaches and high mountains, so I can enjoy both swimming and hiking. I especially like taking the cable car to the top of the mountain because the view of the sea is amazing. Many foreigners also visit Da Nang because it’s a very attractive city.
+* There are many famous places in Vietnam, but my favorite is Da Nang. It has beautiful beaches and high mountains, so I can enjoy both swimming and hiking. I also enjoy many local foods, which were very delicious. The weather was sunny most of the time, so i can enjoy outdoor activities. I especially like taking the cable car to the top of the mountain because the view of the sea is amazing. Because of that, this trip left a strong impression on me.
 
 
-* I had a small problem during a trip with my friends. The bus we took was delayed because of heavy traffic. As a result, we arrived the hotel later than expected. I contacted the hotel staff in advance to explain situation. Luckily, they were very helpful and allowed us to check in late. In the end, the problem was sloved without any serious trouble.
+* I had a small problem during a trip with my friends. The bus we took was delayed because of heavy traffic. As a result, we arrived the hotel later than expected. I contacted the hotel staff in advance to explain situation. Luckily, they were very helpful and allowed us to check in late. In the end, the problem was solved without any serious trouble.
 
 
 * Many people like to travel for different reasons. Some people want to see new places, and others want to make new friends. For me, the best reason to travel is to relieve stress. When I travel, I can relax, enjoy beautiful views, and forget about my busy life. That’s why I think traveling is very important for everyone.
 
 
-* Before traveling, I usually check my schedule and book my hotel and transportation.
-Then, I pack suitable clothes and important personal items.
-I also prepare some basic medicine.
-This helps me feel more comfortable during the trip.
+* Before traveling, I usually check my schedule and book my hotel and transportation. Then, I pack suitable clothes and important personal items. I also prepare some basic medicine. This helps me feel more comfortable during the trip.
 
 
 
