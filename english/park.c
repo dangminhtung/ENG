e525fd64c8fad870737a9b4db80d3712028906b0
dangminhtung <minhtungd2402@gmail.com>
2026-03-01 01:47:35 +0700

    park

--- english/park.c
+++ english/park.c
@@ -30,8 +30,6 @@ forecast: dự báo pho cát
 ! Experience
 * My most memorable experience at the park happened last weekend. I was jogging near a lake in a park close to my house, but I was wearing shoes that were not suitable for jogging. Because I kept paying attention to my shoes, I accidentally slipped while while running and fell down.I hit my knee on the ground and felt a sharp pain. My phone was broken so I couldn't call anyone and I had to go home by myself. Since then, I always make sure to wear the right shoes when I jog.
 
-! Bought book dress
-* It’s important to wear comfortable and light clothes when jogging. I usually wear functional clothes because they absorb sweat well and keep me cool. They also stretch easily, so I can exercise comfortably. I think choosing the right shoes is the most important thing. If the shoes are not comfortable, my feet or ankles can hurt. That’s why I prefer soft and light sneakers when I jog.
 
 ! Dress change
 * When I exercise, sportswear is very important. In the past, I just wore shorts and a T-shirt, but they didn’t absorb sweat, so they were uncomfortable. Now I wear sportswear because they absorb sweat well and stretchy. I also wear soft and light sneakers to protect my feet, so they don’t hurt even after exercising for a long time.
