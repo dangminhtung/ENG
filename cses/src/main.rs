9d3e2c37ccb439547965a5c4041174fbda3fa443
dangminhtung <minhtungd2402@gmail.com>
2026-03-01 14:44:29 +0700

    rust

--- cses/src/main.rs
+++ cses/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, stdin};
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).expect("Failed to read line");
+
+    let mut number: i64 = a.trim().parse().expect("not a number");
+    print!("{} ", number);
+    while number != 1 {
+        if number % 2 == 0 {
+            print!("{} ", number / 2);
+            number = number / 2;
+        } else {
+            print!("{} ", number * 3 + 1);
+            number = number * 3 + 1;
+        }
+    }
 }
