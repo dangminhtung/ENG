c313e1ce32b1d8fe5a031f936363ed03825f0cd7
dangminhtung <minhtungd2402@gmail.com>
2026-03-03 00:27:57 +0700

    music

--- english/Music.c
+++ english/Music.c
@@ -11,10 +11,14 @@
 ! Change
 * I love listening to music. When I was young, I liked fast songs like K-pop because they made me feel excited. But when I entered high school, I started to enjoy ballads because my friend always listened to them. Ballad songs make me feel calm and help me relax after a long day. These days, I like to listen to calm music in the morning to start my day peacefully.
 
-! What kinds of music do you listen to? Who are some of your favorite musicians or composers? 
+! What kinds of music do you listen to? Who are some of your favorite musicians or composers?
 ! Pick 2 different types of music or cmoposers. Describe and compare.
 
-* When it come to music, I enjoy listening to Bruno Mars and TWICE. Bruno Mars is a highly successful solo artist who has won many music awards and topped the Billboard charts with his catchy, trendy pop and R&B songs. he has tons of hit songs that are popular worldwide. TWICE is a successful K-POP girl group with fans all over the world, known for their catchy and trendy music. Both TWICE and Bruno Mars are quite different in terms of genre and style, but both have achieved significant success in the music industry. these are the singers I enjoy listening to.
+* When it come to music, I enjoy listening to Bruno Mars and TWICE. Bruno Mars is a very successful singer. He has won many music awards. Many of his pop and R&B songs are catchy and have been very popular on the charts. TWICE is a successful K-POP girl group with fans all over the world, known for their catchy and trendy music. Both TWICE and Bruno Mars are quite different in terms of genre and style, but both have achieved significant success in the music industry. these are the singers I enjoy listening to.
+
+genre : zan n rờ
+chathy: dễ nhờ cát chy
+significant: có ý nghĩa sig ni fo cừn
 
 
 */
