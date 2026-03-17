89a14ec4edfdebb90ba03e60e60c68a1e74e9914
dangminhtung <minhtungd2402@gmail.com>
2026-03-18 00:20:13 +0700

    toeic

--- TOEIC.c
+++ TOEIC.c
@@ -32,4 +32,32 @@ Who's most qualified for this position?
 
 (C) They filed an official complaint.
 
+
+We will make a final decision about changing the landscaping of the property after reviewing the _____ costs.
+A. estimation
+B. estimate
+C. estimated
+D. estimating
+Đáp án đúng:C
+Giải thích chi tiết đáp án 
+
+Phân tích: Sau mạo từ "the" phải là một (cụm) danh từ. Trong cụm danh từ "the _____ costs", danh từ chính của cụm là "costs" --> cần điền từ bổ nghĩa cho danh từ chính --> ta có thể điền tính từ hoặc danh từ nên bỏ B. Giữa "estimation" (danh từ), "estimated" (đóng vai trò tính từ) và "estimating" (đóng vai trò tính từ), "estimated" hợp nghĩa nhất: những chi phí "được ước tính".
+
+=> Đáp án đúng là C “estimated”.
+
+Dịch câu: Chúng tôi sẽ đưa ra quyết định cuối cùng về chuyện thay đổi việc xây dựng vườn hoa của khu nhà sau khi xem xét chi phí được ước tính.
+
+
+The landlord raised the monthly rent for the first time in several years, and _____ so.
+A. reasonable
+B. reasonably
+C. reason
+D. reasons
+Đáp án đúng:B
+Giải thích chi tiết đáp án 
+
+Phân tích: Cụm "and reasonably so" được thêm vào một câu, sau dấu phẩy có nghĩa là "và như vậy là hợp lý".
+
+Dịch câu: Chủ nhà đã tăng tiền thuê nhà hàng tháng lần đầu tiên trong nhiều năm, và như vậy là hợp lý.
+
 */
