c217efe22371b5072008292dbe80f338d6b0b16f
dangminhtung <79414259+dangminhtung@users.noreply.github.com>
2026-08-25 00:58:14 +0700

    Create Home.c

---
+++ english/Home.c
@@ -0,0 +1,11 @@
+Experienced at home
+I remember when the air conditioner broke down at home. It happened during the summer and it very hot without air conditioner. I called a technician to fix the problem, also I remember arguing with my brother because he was always jumping on my bed. He finally promised not to do that again. Plus, I remember breaking a plate by mistake. I had to clean up the mess. I picked up the big pieces by hand and vacuum smaller pieces. So these are the problem I remember having at home.
+
+A change
+I remember buying new furniture for my bedroom. It was a new bed because the old one was too small for me. You knoe as I got older, I grew taller so I wnet to a furniture store to buy a new bed. After looking at many options, I decided to choose a big brown bed. I had to move the old bed out to make room for the new one. The staff helped me install it. The new bed looked very nice in my room. So that was the change I made in my home
+
+Description
+I have lived in an apartment with my family for few years it has 3 bedrooms. A living room and a kitchen. Beside that there are 2 bathrooms and a big balcony. In the living room, we have some furniture and home appliances. First there is a sofa, a coffee table, a cabinet and a mirror. We also have a tv and an air confitioner. In the kitchen there is a sink, a dining table and some cabinets. We also have a fridge, a microwave and a gas stove. So that's what my place look like.
+
+How I keep my house clean
+There are many things I often do to keep my house clean and tidy. I try to clean my apartment on weekends ot whenever I have time. I'm usually busy on weekdays so I can only vacuum my room and do the dishes. However I deep clean my house on weekends. First I vacuum the floors and clean the furniture, also i clean the bathrooms and toilets. Next I take out the garbage and take a shower. Finally I do the laundry after having a meal. So these are the things I do to keep my house clean
