6d257a78c3a73704d5afbd9bd6a4835af73c83a0
dangminhtung <79414259+dangminhtung@users.noreply.github.com>
2026-09-01 02:37:48 +0700

    Update Home.c

--- english/Home.c
+++ english/Home.c
@@ -1,11 +1,17 @@
+Description
+I have lived in an apartment with my family for few years it has 3 bedrooms. A living room and a kitchen. Beside that there are 2 bathrooms and a big balcony. In the living room, we have some furniture and home appliances. First there is a sofa, a coffee table, a cabinet and a mirror. We also have a tv and an air confitioner. In the kitchen there is a sink, a dining table and some cabinets. We also have a fridge, a microwave and a gas stove. So that's what my place look like.
+
+Favorite room
+My home has several room, including a living room, a kitchen, 2 bedrooms, and a bathroom. Among them, my favorite room is my bedroom. It look simple but is very cozy. There are a bed, a desk, a small bookshelf. I decorated the room with light color, so it feels very comfortable. I usually spent time to reading the books, studying or just relaxing there. That's why I really love my bedroom. That's everything I want to talk about my home and my favorite room
+
 Experienced at home
-I remember when the air conditioner broke down at home. It happened during the summer and it very hot without air conditioner. I called a technician to fix the problem, also I remember arguing with my brother because he was always jumping on my bed. He finally promised not to do that again. Plus, I remember breaking a plate by mistake. I had to clean up the mess. I picked up the big pieces by hand and vacuum smaller pieces. So these are the problem I remember having at home.
+I remember living in a house when i was a kid. It has 3 bedrooms and a big kitchen. I like a backyard and I used to play there all the time with my brother. But now, I live in a big apartment. It has 4 bedrooms and a spacious living room. It is equipped with modern home appliances such as an air purifier and a vacuum cleaner. However it doesn't have a yard so I have to go to the gym in my neighborhood to get some exercise. So these are the things about my childhood home and the place I live in now.
 
 A change
 I remember buying new furniture for my bedroom. It was a new bed because the old one was too small for me. You knoe as I got older, I grew taller so I wnet to a furniture store to buy a new bed. After looking at many options, I decided to choose a big brown bed. I had to move the old bed out to make room for the new one. The staff helped me install it. The new bed looked very nice in my room. So that was the change I made in my home
 
-Description
-I have lived in an apartment with my family for few years it has 3 bedrooms. A living room and a kitchen. Beside that there are 2 bathrooms and a big balcony. In the living room, we have some furniture and home appliances. First there is a sofa, a coffee table, a cabinet and a mirror. We also have a tv and an air confitioner. In the kitchen there is a sink, a dining table and some cabinets. We also have a fridge, a microwave and a gas stove. So that's what my place look like.
-
 How I keep my house clean
 There are many things I often do to keep my house clean and tidy. I try to clean my apartment on weekends ot whenever I have time. I'm usually busy on weekdays so I can only vacuum my room and do the dishes. However I deep clean my house on weekends. First I vacuum the floors and clean the furniture, also i clean the bathrooms and toilets. Next I take out the garbage and take a shower. Finally I do the laundry after having a meal. So these are the things I do to keep my house clean
+
+The problem happened
+As I remember, it happened last summer on a sunday morning. I was cooking breakfast when the pipe under the kitchen sink broke and water valve and moved things away from the sink.  Then, I used a bucket and some towels to stop the leak. After that, I called a plumber, but he was a bit late. While waiting, I used some tape to cover the pipe. Luckily, when the plumber came, he replaced the pipe and fixed everything. Finally, I cleaned the floor and dried the kitchen. In the end, everything was fine. I felt worried at first, but later I was relieved and happy, and the experience reminded me to check the sink regulary. That's all I wanted to talk about regarding that problem.
