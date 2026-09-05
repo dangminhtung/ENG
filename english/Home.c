b62e531389e708f5d8c057fb33e6ef76fb616c5f
dangminhtung <minhtungd2402@gmail.com>
2026-09-05 20:54:55 +0700

    movie and home

--- english/Home.c
+++ english/Home.c
@@ -1,16 +1,15 @@
 /*
-
 ! Description
 * I lived in an apartment for few years. It has 2 bedrooms, a living room and a kitchen. Beside that there are 2 bathrooms and a big balcony. In the living room, we have some furniture and home appliances. First there is a sofa, a coffee table, a cabinet and a mirror. We also have a tv and an air confitioner. In the kitchen there is a sink, a dining table and some cabinets. We also have a fridge, a microwave and a gas stove. So that's what my place look like.
 
 ! Favorite room
-* My home has several room, including a living room, a kitchen, 2 bedrooms, and a bathroom. Among them, my favorite room is my bedroom. It look simple but is very cozy. There are a bed, a desk, a small bookshelf. I decorated the room with light color, so it feels very comfortable. I usually spent time to reading the books, studying or just relaxing there. That's why I really love my bedroom. That's everything I want to talk about my home and my favorite room
+* My home has several room, including a living room, a kitchen, 2 bedrooms, and a bathroom. Among them, my favorite room is my bedroom. It look simple but is very cozy. There are a bed, a desk, a small bookshelf. I decorated the room with light color, so it feels very comfortable. I usually spent time to reading the books, studying or just sleeping there. That's why I really love my bedroom. That's everything I want to talk about my home and my favorite room
 
 ! Experienced at home
-* I remember living in a house when i was a kid. It has 3 bedrooms and a big kitchen. I like a backyard and I used to play there all the time with my brother. But now, I live alone in an apartment. It has 2 bedrooms and a living room. It is equipped with modern home appliances such as an air purifier and a vacuum cleaner. However it doesn't have a yard so I have to go to the gym in my neighborhood to get some exercise. So these are the things about my childhood home and the place I live in now.
+* I remember living in a house when i was a kid. It has 3 bedrooms and a big kitchen. I like a backyard and I used to play there all the time with my brother. But now, I live alone in an apartment. It has 2 bedrooms and a living room. It also has all the same home appliances as the old house. However it doesn't have a yard so I have to go to the gym in my neighborhood to get some exercise. So these are the things about my childhood home and the place I live in now.
 
 ! A change
-* I remember buying new furniture for my bedroom. It was a new bed because the old one was too small for me. You knoe as I got older, I grew taller so I wnet to a furniture store to buy a new bed. After looking at many options, I decided to choose a big brown bed. I had to move the old bed out to make room for the new one. The staff helped me install it. The new bed looked very nice in my room. So that was the change I made in my home
+* I remember buying new furniture for my bedroom. It was a new bed because the old one was too small for me. You know as I got older, I grew taller so I went to a furniture store to buy a new bed. After looking at many options, I decided to choose a big brown bed. I had to move the old bed out to make room for the new one. The staff helped me install it. The new bed looked very nice in my room. So that was the change I made in my home
 
 ! How I keep my house clean
 * There are many things I often do to keep my house clean and tidy. I try to clean my apartment on weekends ot whenever I have time. I'm usually busy on weekdays so I can only vacuum my room and do the dishes. However I deep clean my house on weekends. First I vacuum the floors and clean the furniture, also i clean the bathrooms and toilets. Next I take out the garbage and take a shower. Finally I do the laundry after having a meal. So these are the things I do to keep my house clean
