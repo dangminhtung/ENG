674899d4c009f00aa58876688714522964958dcb
dangminhtung <minhtungd2402@gmail.com>
2026-09-06 04:42:17 +0700

    movie and home

--- english/movie.c
+++ english/movie.c
@@ -1,6 +1,6 @@
 /*
 ! the movie that you watched recently?
-* I recently watched Avengers: Endgame. It was a very exciting movie with a lot of action and special effects. I enjoyed watching all the superheroes fight together to save the world. Some parts of the movie were really emotional, especially the ending.   one of the most impressive movies I have ever watched.
+* I recently watched Avengers: Endgame. It was a very exciting movie with a lot of action and special effects. I enjoyed watching all the superheroes fight together to save the world. Some parts of the movie were really meaningfull, especially the ending. One of the most impressive movies I have ever watched.
 
 ! Description
 * In my free time I often go to a cinema called CGV to watch movies. The cinema is close to my house so it only takes me a few minutes to get there. The building is large with bright colors outside and warm colors inside. The armchairs are soft and very comfortable which makes the movie experience better. The popcorn and Coke are also delicious so I usually buy them before the movie starts. I also like services there, especially the online ticket booking which is very convenient. So that's all about my favorite cinema. 
@@ -9,6 +9,7 @@
 * When it comes to movies, I usually enjoy action films and Korean movies. Action movies are really exciting because there are many fast and fun action scenes, also action films these days are very well made so they are always my first choice. When I go to the theater I also like Korean movie, they often talk about family and relationships and many of them are touching and meaningful, because of that I enjoy watching them with my family especially on weekends. So overall action movies and Korean movies are my favorite types of movies
 
 ! Memorable 
-* I remember I going to the cinema last month with my friends. It took us around 30 minutes to get there because the theater is near the city center. Before the movie started I booked our tickets online which was very convenient. Then when we arrived, I bought some popcorn and a Coke for the movie. The movie itself was amazing. It was actually my wife's favorite movie so I was excited to watch it. The man characters were good looking and very talented and the story was fun to follow. We really enjoyed the whole experience. After the movie, we went to the restaurant nearby. We ordered some food and talked a lot about the movie. Overall it was a very pleasant evening for all of us
+* I remember I going to the cinema last month with my friends. It took us few minutes to get there because the cinema is near my house. Before the movie started I booked our tickets online which was very convenient. Then when we arrived, I bought some popcorn and a Coke for the movie. The movie itself was amazing. The man characters were good looking and very talented and the story was fun to follow. We really enjoyed the whole experience. After the movie, we went to the restaurant nearby. We ordered some food and talked a lot about the movie. Overall it was a memorable evening for all of us
+
 
 */
