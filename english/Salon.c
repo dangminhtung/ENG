798832ea775292ea358b6570eceae8a20b3d27d6
dangminhtung <minhtungd2402@gmail.com>
2026-09-15 01:29:58 +0700

    hair salon and appointments

---
+++ english/Salon.c
@@ -0,0 +1,15 @@
+/*
+
+! Please talk about the hair salon or barbershop you usually go to. Where is it located? What dose it look like? Please explain in detail.
+* I usually go to a small barbershop near my home. It is about ten minutes away by motorbike. The shop is not very big, but it is clean and comfortable. There are several chairs, big mirrors, and some pictures of hairstyles. There is also a TV and some hair-cutting equipment. The barbers are friendly and do their job very well.
+
+! What do you usually do when you go to the hair salon? Please talk about the things you normally you do there
+* When I go to the barbershop, I usually get a haircut. First, I tell the barber what kind of hairstyle I want. Then, I sit in the chair and look at the mirror. The barber cuts my hair and sometimes washes it. The whole process usually takes about thirty minutes. After that, I pay and go home.
+
+! What was your hairstyle like when you were young? How is it different from now?
+* When I was young, I had very short hair. My mother usually took me to the barbershop. I did not care much about my hairstyle at that time. My hair was simple, and I usually had the same haircut. Now, I pay more attention to my hairstyle. I usually choose a neat and modern haircut.
+
+! Please talk about hair salon in your country. Where are they usually located? What do they look like?
+* In Vietnam, barbershops are usually located near homes, markets, and busy streets. There are many small barbershops in almost every neighborhood. Some shops are simple, with only a few chairs and mirrors. Other shops are modern and have air conditioning and comfortable seats. The price is usually reasonable, so many men go there regularly. People often go to a barbershop once or twice a month.
+
+*/
