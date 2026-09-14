09c9f94e423fdff8160c2f38faf069424375392c
dangminhtung <minhtungd2402@gmail.com>
2026-09-15 01:17:21 +0700

    park and music

---
+++ english/Appointment.c
@@ -0,0 +1,14 @@
+/*
+! What kind of appointment do you make in your life? Where do you go to make your appointment?
+* I usually make appointments for the doctor or dentist. Sometimes, I also make an appointment at a hair salon. For a doctor’s appointment, I usually call the hospital or use an app. For a haircut, I call the salon and choose a convenient time. I usually make an appointment when I am free.
+
+! What kind of thing do you do when you make appointment? Tell me what you do exactly when you make these appointments?
+* First, I decide what kind of appointment I need. Then, I check my schedule and choose a good time. After that, I call the place or use an app. I tell them my name and the reason for my appointment. Finally, I check the date and time again. I usually save the appointment on my phone.
+
+! Talk about an appointment you made as a child? What was appointment for? Was it for a doctor, dentis or a new school? What did you do and what happened when you got to your appointment?
+* When I was a child, I had a doctor’s appointment. I had a fever and did not feel well. My mother made an appointment at a local hospital. On that day, we went to the hospital by bus. The doctor checked my temperature and gave me some medicine. After a few days, I felt much better.
+
+! Unexpected things can happen when you make appointment. Talk about memorable incident regarding an appointment. What exatly happened and how did you deal with the situation?
+* I remember one appointment at a hospital. I made the appointment for 10 a.m., but I arrived late. There was a lot of traffic on the way to the hospital. I called the hospital and explained the situation. Luckily, they understood me and changed the appointment time. After that, I always leave home early for an appointment.
+
+*/
